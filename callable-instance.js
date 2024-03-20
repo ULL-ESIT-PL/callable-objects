9d8f47c30c56d545ebf213dedbc652e9966f37ca
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2024-03-20 16:41:50 +0000

    callable-instance.js

--- callable-instance.js
+++ callable-instance.js
@@ -5,11 +5,11 @@ class ArrayFunction extends CallableInstance {
   constructor(...a) {
     // CallableInstance accepts the name of the property to use as the callable
     // method.
-    super("instanceMethod");
+    super("_call");
     this.array = a;
   }
  
-  instanceMethod(arg) {
+  _call(arg) {
     let result = this.array[arg];
     //console.log(arg)
     //console.log(this.array);
@@ -23,18 +23,18 @@ class ArrayFunction extends CallableInstance {
  
 var test = new ArrayFunction(0,1,2);
 // Invoke the method normally
-let r = test.instanceMethod(1);
-console.log(r);
-// Call the instance itself, redirects to instanceMethod
+let r = test._call(1);
+console.log("test._call(1) = ",r);
+// Call the instance itself, redirects to _call
 r = test(0);
-console.log(r);
+console.log("test(0)=", r);
 // The instance is actually a closure bound to itself and can be used like a
 // normal function.
 r = test.apply(null, [2]);
-console.log(r);
+console.log("test.apply(null, [2]) = ",r);
 
 r = test("length");
-console.log(r);
+console.log('test("length") = ', r);
 
 r  = test("concat")(9, 5, 7);
-console.log(r);
+console.log('test("concat")(9, 5, 7) = ',r);
